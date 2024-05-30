90d715d7f79df937f6c8f6be5f8db632990ddb93
sabrinatouch <sabrinajindatouch@gmail.com>
2024-05-29 22:04:46 -0600

    Import font

--- src/style.tsx
+++ src/style.tsx
@@ -1,9 +1,13 @@
-import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
 import theme from "./shared/theme/index";
 
 export default createGlobalStyle`
     html {
         background-color: ${theme.bg.darkPurple};
+        color: ${theme.text.default};
+    }
+
+    body {
+        font-family: "Jersey 15", sans-serif;
     }
 `;
