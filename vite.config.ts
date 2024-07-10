8029da0fdf1c3557191eb034fd22c12ada2bcdc4
sabrinatouch <sabrinajindatouch@gmail.com>
2024-07-09 21:30:19 -0600

    Remove minor redline errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      include: "**/*.tsx",
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/setupTests.js'],
+    setupFiles: ['./src/setupTests.ts'],
   },
 });
