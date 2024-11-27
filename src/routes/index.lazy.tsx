2b2ea7ce45484672ab37ba943566c3669e6d1a9b
sabrinatouch <sabrinajindatouch@gmail.com>
2024-11-27 16:50:09 -0700

    Setup axios calls, display data

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,19 +1,65 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 import Card from '../components/Card'
 import Category from '../components/Category'
-import data from '../../public/data.json'
+import axios, { AxiosInstance } from 'axios'
+import { useEffect, useState } from 'react';
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
 })
 
+interface Category {
+  id: number;
+  name: string;
+}
+
+interface Subject {
+  id: number;
+  categoryId: number;
+  name: string;
+  wins: number;
+  losses: number;
+}
+
+const apiClient: AxiosInstance = axios.create({
+  baseURL: 'http://localhost:3000',
+  timeout: 5000,
+  headers: { 'Content-Type': 'application/json' },
+});
+
 function Index() {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [subjects, setSubjects] = useState<Subject[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const categoriesResponse = await apiClient.get<Category[]>('/categories');
+        console.log(categoriesResponse.data); // Trigger the Axios call
+        setCategories(categoriesResponse.data);
+
+        const subjectsResponse = await apiClient.get<Subject[]>('/subjects');
+        console.log(subjectsResponse.data); // Trigger the Axios call
+        setSubjects(subjectsResponse.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        return error;
+      }
+    };
+
+    getData(); // Trigger the Axios call when the component mounts
+  }, []);
+
   return (
     <div className="">
-      <Category text={data.data[0].category} />
+      {categories.map((category) => (
+        <Category key={category.id} text={category.name} />
+      ))}
       <div className="flex justify-evenly items-center flex-col md:flex-row my-10 md:my-28 gap-4 md:gap-0">
         <div className="w-4/5 md:w-1/3">
-          <Card text={data.data[0].options[0]} />
+          {subjects.find((subject) => subject.id === 1) && (
+            <Card text={subjects.find((subject) => subject.id === 1)?.name || ''} />
+          )}
         </div>
         <div className="w-4/5 md:w-1/3 text-center">
           <span className="text-6xl">
@@ -21,7 +67,9 @@ function Index() {
           </span>
         </div>
         <div className="w-4/5 md:w-1/3">
-          <Card text={data.data[0].options[1]} />
+          {subjects.find((subject) => subject.id === 2) && (
+            <Card text={subjects.find((subject) => subject.id === 2)?.name || ''} />
+          )}
         </div>
       </div>
     </div>
