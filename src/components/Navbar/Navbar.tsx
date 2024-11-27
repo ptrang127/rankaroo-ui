c5a2af61536bf0365cc1d7aa46213e8a0419b044
sabrinatouch <sabrinajindatouch@gmail.com>
2024-11-27 15:08:39 -0700

    Fix console errors

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,27 +5,21 @@ export const Navbar: FC<{}> = () => {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <a className="btn btn-ghost btn-lg text-5xl uppercase">
                 <Link to="/">
                     Rankaroo
                 </Link>
-                </a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1 text-base font-mono">
                     <li>
-                    <a>
                         <Link to="/insights" className="[&.active]:underline">
                         Insights
                         </Link>
-                    </a>
                     </li>
                     <li>
-                    <a>
                         <Link to="/about" className="[&.active]:underline">
                         About
                         </Link>
-                    </a>
                     </li>
                 </ul>
             </div>
