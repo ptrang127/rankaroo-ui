cee30b84c0c8aef377e8637c4eb4fa411945b398
sabrinatouch <sabrinajindatouch@gmail.com>
2024-11-26 19:30:37 -0700

    Responsive layout for cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const Card: FC<Props> = (props) => {
     return (
-        <div className="bg-violet-600 hover:bg-violet-800 hover:scale-105 text-6xl w-fit h-fit py-3 px-6 rounded-lg">
+        <div className="bg-violet-600 hover:bg-violet-800 hover:scale-105 text-4xl h-auto py-3 px-6 rounded-lg text-pretty text-center">
             {props.text}
         </div>
     )
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -8,14 +8,20 @@ export const Route = createLazyFileRoute('/')({
 
 function Index() {
   return (
-    <div>
+    <div className="">
       <Category text={'actors'} />
-      <div className="p-2 columns-3 flex justify-around items-center">
-        <Card text={'option1'} />
-        <span className="text-6xl">
-          vs
-        </span>
-        <Card text={'option2'} />
+      <div className="flex justify-evenly items-center flex-col md:flex-row my-6 md:my-24 gap-4 md:gap-0">
+        <div className="w-4/5 md:w-1/3">
+          <Card text={'This is an option with a lot of text. This is an option with a lot of text. This is an option with a lot of text. This is an option with a lot of text. This is an option with a lot of text.'} />
+        </div>
+        <div className="w-4/5 md:w-1/3 text-center">
+          <span className="text-6xl">
+            vs
+          </span>
+        </div>
+        <div className="w-4/5 md:w-1/3">
+          <Card text={'option2'} />
+        </div>
       </div>
     </div>
   )
