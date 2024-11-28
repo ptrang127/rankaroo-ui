d02224c8d7e10e843ea7a93a8eaa3340633ad19d
Sabrina Touch <36649419+sabrinatouch@users.noreply.github.com>
2024-11-28 15:15:53 -0700

    Setup axios (#12)
    * Fix console errors
    
    * Setup axios calls, display data
    
    * Fix navbar logo
    
    * Add basic employee info
    
    * Rename categoryId to category_id

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -5,5 +5,24 @@ export const Route = createLazyFileRoute('/about')({
 })
 
 function About() {
-  return <div className="p-2">This is the About page.</div>
+  return (
+    <div className="px-8 flex flex-col my-10 gap-6">
+      <div className="text-left">
+        <h1 className="text-5xl">ponyboy</h1>
+        <p className="font-mono">Product Manager, Lead Engineer</p>
+      </div>
+      <div className="text-left">
+        <h1 className="text-5xl">bk philip</h1>
+        <p className="font-mono">Staff Engineer</p>
+      </div>
+      <div className="text-left">
+        <h1 className="text-5xl">sabbages</h1>
+        <p className="font-mono">Front-End Engineer</p>
+      </div>
+      <div className="text-left">
+        <h1 className="text-5xl">genghsta</h1>
+        <p className="font-mono">Intern</p> 
+      </div>       
+    </div>
+  )
 }
